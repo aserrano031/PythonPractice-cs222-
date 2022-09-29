b052de9c601952531acca0baf3dff133e40a8b9c
Alexis Serrano <aserrano031@github.com>
2022-09-29 01:59:14 -0500

    new speech to text file

---
+++ source/speech-text.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = r.listen(source)
+
+    try: 
+        text = r.recognize_google(audio)
+        print('{}'.format(text))
+    except:
+        print("Oh no. Try again")
